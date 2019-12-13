9081747b864aa11c839427edce6d9bcf3ec6bf17
Chris Marsh <chrismarsh.c2@gmail.com>
2019-12-13 15:27:02 -0600

    spello

--- test_package/test.cpp
+++ test_package/test.cpp
@@ -3,7 +3,7 @@
 
 
 int main (void) {
-    doube Rh = 70;
+    double Rh = 70;
     double Ta = -15;
     double Td = mio::Atmosphere::RhtoDewPoint(Rh/100.0,Ta+273.15,false);
     printf ("Td = %d\n", Td);
