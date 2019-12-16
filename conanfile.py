83f0d17bedeb7d72d93efc32cb9ee7a0b8de9807
Chris Marsh <chrismarsh.c2@gmail.com>
2019-12-16 10:16:06 -0600

    update build and test code

--- conanfile.py
+++ conanfile.py
@@ -114,7 +114,7 @@ class ProjConan(ConanFile):
         self.copy("*.so", dst="lib", keep_path=False)
         self.copy("*.dylib", dst="lib", keep_path=False)
         self.copy("*.lib", dst="lib", keep_path=False)
-        self.copy("libmeteoio*.a", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
 
     def package_info(self):
         self.cpp_info.includedirs = ["include"]
--- test_package/test.cpp
+++ test_package/test.cpp
@@ -6,6 +6,6 @@ int main (void) {
     double Rh = 70;
     double Ta = -15;
     double Td = mio::Atmosphere::RhtoDewPoint(Rh/100.0,Ta+273.15,false);
-    printf ("Td = %d\n", Td);
+    printf ("Td = %g\n", Td);
     return 0;
 }
